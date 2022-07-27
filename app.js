47469ca882e8b58a0a77c9c31c22130617c430aa
Emmanuel Zelarayan <emmavz@live.com>
2022-07-27 16:25:47 -0300

    authRoutes, authController and User model(register, login and logout)

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log('E-Commerce API');
+// dotenv
+require("dotenv").config();
+
+// async errors
+require("express-async-errors");
+
+// express
+const express = require("express");
+const app = express();
+
+// rest of the packages
+const morgan = require("morgan");
+
+// database
+const connectDB = require("./db/connect");
+
+// routers
+const authRouter = require("./routes/authRoutes");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+    res.send("e-commerce api");
+})
+
+app.use("/api/v1/auth", authRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
